5e3df5ed3c3b2ce5cada8306c3919b8955a1b25a
deathcap <deathcap@gmx.co.uk>
2016-01-18 21:27:31 -0800

    Add bedrock block

---
+++ bedrock.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(game, opts) {
+  return new BedrockPlugin(game, opts);
+};
+module.exports.pluginInfo = {
+  loadAfter: ['voxel-registry']
+};
+
+function BedrockPlugin(game, opts) {
+  this.registry = game.plugins.get('voxel-registry');
+
+  if (!this.registry) throw new Error('voxel-bedrock requires voxel-registry plugin');
+
+  this.enable();
+};
+
+BedrockPlugin.prototype.enable = function() {
+  this.registry.registerBlock('bedrock', {texture: 'bedrock'});
+};
+
+BedrockPlugin.prototype.disable = function() {
+  // TODO: unregister block
+};
